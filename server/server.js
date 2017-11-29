a1b667fc110356280bca3ebc80e8d5d703618f1c
Sheldon <scoates5@uwo.ca>
2017-11-29 05:05:20 +0000

    encrypted password

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var user = require('./models/user');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var saltRounds = 10;
 mongoose.connect('mongodb://localhost:27017/nasa'); 
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,13 +22,6 @@ router.use(function(req, res, next) {
     next();
 });
 
-// GET request to /api returns { message: 'Hello World' }
-// In my C9 account the request must be sent to https://node-angular-lgobinath.c9users.io:8081/api
-router.get('/', function(req, res) {
-    res.json({ message: 'Hello World' });
-});
-
-
 router.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
@@ -39,9 +35,9 @@ router.post('/login', function(req, res){
             return res.status(404).send();
         }
         return res.status(200).send();
-    })
+    });
     
-})
+});
 
 router.post('/register', function(req, res){
     var username = req.body.username;
@@ -50,14 +46,19 @@ router.post('/register', function(req, res){
     var newUser = new user();
     newUser.username = username;
     newUser.password = password;
-    
-    newUser.save(function(err, savedUser){
-        if (err) {
-            console.log(err);
-            return res.status(500).send();
-        }
-        return res.status(200).send();
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash){
+            newUser.password = hash;
+            newUser.save(function(err, savedUser){
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send();
+                }
+                return res.status(200).send();
+            });
+        });
     });
+    
 });
 
 
