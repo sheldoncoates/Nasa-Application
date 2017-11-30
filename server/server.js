fc50f2832b52c8f14a36a13d4c6b2a2c88357991
Sheldon <scoates5@uwo.ca>
2017-11-30 02:07:27 +0000

    need to send verification email

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var user = require('./models/user');
 var mongoose = require('mongoose');
+var sanitizeHtml = require('sanitize-html');
 var bcrypt = require('bcrypt');
 var saltRounds = 10;
 mongoose.connect('mongodb://localhost:27017/nasa'); 
@@ -25,40 +26,58 @@ router.use(function(req, res, next) {
 router.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
-    
-    user.findOne({username: username, password: password}, function(err, user){
+
+    user.findOne({username: username}, function(err, user){
+        var response = res;
         if(err){
             console.log(err);
-            return res.status(500).send();
+            return res.status(500).send("error");
         }
         if(!user){
-            return res.status(404).send();
+            return res.status(404).send("user does not exist");
         }
-        return res.status(200).send();
+        var hashPassword = user.password;
+        bcrypt.compare(password, hashPassword, function(err, res){
+            if(res == true){
+                return response.status(200).send("user has been found with matching password");
+            }
+            else{
+                return response.status(404).send("password does not match username");
+            }
+        });
     });
     
 });
 
 router.post('/register', function(req, res){
-    var username = req.body.username;
-    var password = req.body.password;
-    
-    var newUser = new user();
-    newUser.username = username;
-    newUser.password = password;
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(newUser.password, salt, function(err, hash){
-            newUser.password = hash;
-            newUser.save(function(err, savedUser){
-                if (err) {
-                    console.log(err);
-                    return res.status(500).send();
-                }
-                return res.status(200).send();
-            });
-        });
+    var username = sanitizeHtml(req.body.username, {
+        allowedTags: [],
+        allowedAttributes: []
+    });
+    var password = sanitizeHtml(req.body.password, {
+        allowedTags: [],
+        allowedAttributes: []
     });
     
+    if(username == "" || password == ""){
+        res.status(400).send("injection detected");
+    }else{
+        var newUser = new user();
+        newUser.username = username;
+        newUser.password = password;
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(newUser.password, salt, function(err, hash){
+                newUser.password = hash;
+                newUser.save(function(err, savedUser){
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).send();
+                    }
+                    return res.status(200).send();
+                });
+            });
+        });
+    }
 });
 
 
